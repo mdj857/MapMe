dca5dcc6613fe3fccb9f92eb263aa7ede5c77c49
Matthew Johnson <mjohnson082396@utexas.edu>
2018-03-27 13:48:14 -0500

    added MainActivity and Buttons

--- app/src/main/java/com/example/mapme_hw4/MainActivity.java
+++ app/src/main/java/com/example/mapme_hw4/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.mapme_hw4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText address;
+    private Button submit_address_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        address = (EditText) findViewById(R.id.address_text);
+        submit_address_btn = (Button) findViewById(R.id.submit_address_btn);
+
+        submit_address_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+                address.setText("YOLO");
+            }
+        });
+
+
+
     }
 }
