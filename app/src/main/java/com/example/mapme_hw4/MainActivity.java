f1e063f77bf35a7a2a42fa0e888098daacd8c0ee
Matthew Johnson <mjohnson082396@utexas.edu>
2018-03-27 15:22:43 -0500

    added geocoder code

--- app/src/main/java/com/example/mapme_hw4/MainActivity.java
+++ app/src/main/java/com/example/mapme_hw4/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mapme_hw4;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +10,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText address;
+    private EditText address_edit_text;
     private Button submit_address_btn;
 
     @Override
@@ -16,14 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        address = (EditText) findViewById(R.id.address_text);
+        address_edit_text = (EditText) findViewById(R.id.address_text);
         submit_address_btn = (Button) findViewById(R.id.submit_address_btn);
 
         submit_address_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO
-                address.setText("YOLO");
+                //address_edit_text.setText("YOLO");
+                String address = address_edit_text.getText().toString();
+
+                // actual--see https://stackoverflow.com/questions/3574644/how-can-i-find-the-latitude-and-longitude-from-address
+                Intent searchAddress = new  Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q="+address));
+                startActivity(searchAddress);
             }
         });
 
